e0b35bfe2d45766313a8baea391f875894e7eeb2
ljs614 <sjl0614@naver.com>
2020-03-22 20:04:07 +0900

    start React Style

---
+++ awesomeness/src/App.js
@@ -0,0 +1,11 @@
+import React, {Component, Fragment} from 'react';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <button className="button">Hello</button>;
+    
+  }
+}
+
+export default App;
