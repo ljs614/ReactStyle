7ecc7fad457be1489dd71fdd6a0ed3d585a83ab5
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-03-29 20:24:03 +0900

    styled components

--- awesomeness/src/App.js
+++ awesomeness/src/App.js
@@ -1,11 +1,37 @@
-import React, {Component, Fragment} from 'react';
-import "./App.css";
+import React, {Component} from 'react';
+import styled from "styled-components";
 
 class App extends Component {
   render() {
-    return <button className="button">Hello</button>;
-    
+    return (
+      <Container>
+        <Button>Hello</Button>
+        <Button danger>Hello</Button>
+      </Container>
+    );
   }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+`
+
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`
 
 export default App;
