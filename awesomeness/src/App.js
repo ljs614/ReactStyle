12ce33b5d781a8abba7e35144ed0d987b6442c30
ljs614 <sjl0614@naver.com>
2020-03-30 21:59:30 +0900

    GlobalStyle, Animation

--- awesomeness/src/App.js
+++ awesomeness/src/App.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
-import styled from "styled-components";
+import styled, {createGlobalStyle, css,  keyframes} from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
+      <GlobalStyle />
       <Container>
         <Button>Hello</Button>
-        <Button danger>Hello</Button>
+        <Button danger rotationTime={5}>Hello</Button>
+        <Anchor as="a" href="http://google.com">Go to goole</Anchor>
       </Container>
+      </React.Fragment>
     );
   }
 }
@@ -16,7 +27,7 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #bdc3c7;
-`
+`;
 
 
 const Button = styled.button`
@@ -32,6 +43,24 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
-`
+  ${props => {
+    if(props.danger){
+      return css`animation: ${rotation} ${props.rotationTime}s linear infinite`;
+    }
+  }}
+`;
+
+const Anchor = styled(Button)`
+  text-decoration: none;
+`;
+
+const rotation = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`;
 
 export default App;
