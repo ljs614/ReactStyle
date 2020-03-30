bcfbee718e26aca460d7d6674fa48ac30b3bc15f
ljs614 <sjl0614@naver.com>
2020-03-30 22:19:20 +0900

    Theming

--- awesomeness/src/App.js
+++ awesomeness/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import styled, {createGlobalStyle, css,  keyframes} from "styled-components";
+import styled, {createGlobalStyle, ThemeProvider} from "styled-components";
+import theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -8,39 +9,44 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-	padding: 20px;
-`;
-
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #bdc3c7;
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  ${awesomeCard};
+const Card = styled.div`
+  background-color: white;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <GlobalStyle />
-      <Container>
-        <Input placeholder="hello" />
-      </Container>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Container>
+            <Form />
+          </Container>
+        </ThemeProvider>
       </React.Fragment>
     );
   }
 }
 
+const Form = () => (
+  <Card>
+    <Button>
+      Hello
+    </Button>
+  </Card>
+);
 
 
 export default App;
